fb62adfc01f2fb70632f3dc7c1f32e2a316e2596
Danny <xi4869@gmail.com>
2021-11-30 16:05:43 +0800

    finish ruchan page and finish click event

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,6 @@
 
 let getFooter = document.querySelector('.footer');
 getFooter.innerHTML = `
+<div>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="蒙尼克">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 <p>  Copyright ©2021柴知道. All rights reserved.</p>
 `;
--- js/ruchan.js
+++ js/ruchan.js
@@ -1,65 +1,42 @@
-        let num ;
-        let getWrapPhoto = document.querySelector(".wrapPhoto");
-        let getPhotoBox = document.getElementsByClassName("photoBox");
-        
-        let getLi = document.querySelectorAll(".wrapPhoto>li");
-    
-        // let getBody = document.querySelector(body);
-        // for(i=0; i<getPhotoBox.length; i++){
-        //    getPhotoBox[i].dataset.photoNum = i;
-           
-        // }
-        // console.log(getPhotoChange)
-        let modalOpen = false;
-        getWrapPhoto.addEventListener("click", function(e){
-            // console.log(e.target.nodeName);
-            if(modalOpen==true||e.target.nodeName=="UL"){return}
-            if(modalOpen==true||e.target.nodeName=="LI"){return}
-            for(let i=0;i<getPhotoBox.length; i++){
-                if(e.target.parentNode==getPhotoBox[i]){
-                     num=i;
-                     break;
-                };
-            }
-         
-            let copyNode = getPhotoBox[num].cloneNode(true);
-            copyNode.classList.add("photoChange");
-            // console.log(copyNode);
-            getBody.appendChild(copyNode);
-            let getPhotoChange = document.getElementsByClassName("photoChange");
-            var styleSheets = document.styleSheets[0];  //獲取樣式表引用
-            // console.log(styleSheets);
-            var index = styleSheets.length;  //獲取樣式表中包含樣式的個數
-            styleSheets.insertRule(`body::before{display: block;position: fixed;
-                content: "";
-                top: 0;
-                left: 0;
-                background-color: rgba(0, 0, 0, 0.651);
-                width: 100%;
-                height: 100%;
-                z-index: 100;}`, index);
+let getPhotoBox = document.getElementsByClassName("wrapPhoto")[0];
+const getFixed = document.querySelector(".fixed");
+const getClickedBox = document.querySelector(".clickedBox");
+getPhotoBox.addEventListener("click",function(e){
+    console.log("get");
+    if(e.target.nodeName=="LI"){return}
+    if(e.target.nodeName=="UL"){return}
+    const obj ={};
+    obj.src = e.target.parentNode.childNodes[1].getAttribute("src");
+    obj.h2 = e.target.parentNode.childNodes[3].textContent;
+    obj.p = e.target.parentNode.childNodes[5].textContent;
+    getClickedBox.childNodes[1].setAttribute("src", obj.src);
+    getClickedBox.childNodes[3].textContent = ("src", obj.h2);
+    getClickedBox.childNodes[5].textContent = ("src", obj.p);
+    getFixed.style.display = "block";
+});
 
-            window.setTimeout(function(){
-                getPhotoChange[0].style.width = "80vw";
-                modalOpen=true;
-                // console.log(getPhotoChange[0]);
-                // console.log("1");
-            }, 1)
-        },true);
-        document.addEventListener("click", e => {
-            console.log(modalOpen);
-            console.log(e.target);
-            if(modalOpen==true){
-                var styleSheets = document.styleSheets[0];  //獲取樣式表引用
-                
-                console.log(styleSheets);
-                styleSheets.deleteRule(0);
-                let t  =  document.getElementsByClassName("photoChange");
-                // t[0]
-                t[0].innerHTML = "";
-                var parentObj = t[0].parentNode;
-                parentObj.removeChild(t[0]);
-                modalOpen=false;
-            }
+getFixed.addEventListener("click", function() {
+    if( getFixed.style.display == "block"){
+        getFixed.style.display ="none";
+    }
+})
 
-          });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var el = document.querySelector(".wrapPhoto");
+// console.log(el);
+// el.addEventListener("click", function (e) {
+
+//     console.log(e);
+// }, false);
